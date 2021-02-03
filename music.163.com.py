5ac50bcb615eda5f27d75aeb1f5ba5c4c6958ded
96bearli <53075597+96bearli@users.noreply.github.com>
2021-02-03 22:31:20 +0800

    mp3_info

--- music.163.com.py
+++ music.163.com.py
@@ -2,7 +2,11 @@
 import re
 import requests
 import os
-
+import eyed3
+# 想要继续实现的功能
+# 音乐详细信息 > 标题 艺术家已实现
+# 内嵌专辑图片
+# 内嵌lrc歌词
 headers = {
     'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 12239.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.102 Safari/537.36'}
 
@@ -14,7 +18,8 @@ def get_ids(key_word):
     # print(response)
     # data = re.findall(re.compile('"id": (\d+?),\n +?"name": "(.+?)",\n +?"artists"'), response)
     # 这个正则匹配的文本有重复性，所以从两侧开始写正则，中间用特殊的部分分割上下
-    find_data = re.compile('{.+?"id": (\d+?),.+?"name": "(.+?)",.+?"artists".+?\{.+?"id".+?"name": "(.+?)",.+?"album":.+?},', re.S)
+    find_data = re.compile(
+        '{.+?"id": (\d+?),.+?"name": "(.+?)",.+?"artists".+?\{.+?"id".+?"name": "(.+?)",.+?"album":.+?},', re.S)
     data = re.findall(find_data, response)
     # print(len(data))
     # print(data)
@@ -24,17 +29,32 @@ def get_ids(key_word):
     return data
 
 
+def song_info(alist):
+    audiofile = eyed3.load('./cache/' + alist[1] + "_" + alist[2] + '.mp3')  # 读取mp3文件
+    audiofile.initTag()  # 初始化所有标签信息，将之前所有的标签清除
+    audiofile.tag.artist = alist[2]  # 参与创作的艺术家
+    # audiofile.tag.album = u"Love Visions"  # 唱片集
+    # audiofile.tag.album_artist = u"art"  # 唱片艺术家
+    audiofile.tag.title = alist[1]  # 标题
+    audiofile.tag.track_num = 4  # 音轨编号，专辑内歌曲编号："#"
+    audiofile.tag.save()  # 保存文件
+
 def get_song(alist):
     url = "http://music.163.com/song/media/outer/url?id=" + alist[0] + ".mp3"
-    print("下载链接为:" + url, "\n开始下载")
+    print("url:" + url, "\nBegin to download!")
     response = requests.get(url=url, headers=headers).content
     try:
         os.mkdir("./cache")
     except Exception as e:
-        print("-"*20)
-    with open('./cache/' + alist[1]+"_"+alist[2] + '.mp3', "wb") as f:
+        print("-" * 20)
+    with open('./cache/' + alist[1] + "_" + alist[2] + '.mp3', "wb") as f:
         f.write(response)
-    print("Done!Please check './cache'")
+    print("Get it!Begin to write Song_info")
+    try:
+        song_info(alist)
+    except:
+        print("* Song_info failed to change")
+    print("Done!Please check ./cache" + alist[1] + "_" + alist[2] + '.mp3')
 
 
 if __name__ == '__main__':
@@ -43,5 +63,9 @@ if __name__ == '__main__':
     print("根据以下列表，您要获取的music为？")
     for id_name in id_name_list:
         print("No." + str(id_name_list.index(id_name)), id_name[1], id_name[2], sep="\t")
-    count = int(input("No."))
+    try:
+        count = int(input("No."))
+    except:
+        print("default No.0")
+        count = 0
     get_song(id_name_list[count])

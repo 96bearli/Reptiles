7b79d21ab7e8fae2da87c36078e97ab43645d18d
96bearli <53075597+96bearli@users.noreply.github.com>
2021-12-24 22:10:22 +0800

    Update fanyi.baidu.py
    from re to json

--- fanyi.baidu.py
+++ fanyi.baidu.py
@@ -1,8 +1,5 @@
 #!python
 import requests
-import json
-import re
-
 
 def translate():
     post_url = "https://fanyi.baidu.com/sug"
@@ -11,29 +8,26 @@ def translate():
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12239.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.102 Safari/537.36"}
     try:
-        response = requests.post(url=post_url, data=data, headers=headers)
+        response = s.post(url=post_url, data=data, headers=headers)
     except:
         print("未找到释义！")
         return
-    dic_obj = str(response.json())
-    # print(dic_obj)
-    # findk = re.compile(r'"k": "(.*?)"')
-    findk = re.compile(r"'k': '(.*?)'")
-    keys = re.findall(findk, dic_obj)
-    # findv = re.compile(r'"v": "(.*?)"')
-    findv = re.compile(r"'v': '(.*?)'")
-    values = re.findall(findv, dic_obj)
+    obj = response.json()['data']
     print("-" * 20)
-    if len(keys) == 0:
+    if len(obj) == 0:
         print("无释义")
         print('-' * 20)
-        return 
-    for i in range(len(keys)):
-        print("【" + keys[i] + "】:\t" + values[i])
+        return
+    for kv in obj:
+        print("【" + kv['k'] + "】:\t" + kv['v'])
     print("-" * 20)
     print("结果来自百度翻译")
-i = 0
-while True:
-    i += 1
-    print("当前为第%d次查询"%i)
-    translate()
+
+
+if __name__ == '__main__':
+    i = 0
+    s = requests.session()
+    while True:
+        i += 1
+        print("当前为第%d次查询" % i)
+        translate()
